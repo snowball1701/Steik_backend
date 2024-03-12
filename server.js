51e469fafad8cc902046efa2ec3d342c5f79b93b
philljohnson <philljohnson739@gmail.com>
2024-03-12 02:04:47 -0300

    feat: initial simple backend

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const { getCosmWasmClient } = require("@sei-js/core");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const cosmWasmClient = getCosmWasmClient(
+  "https://sei-testnet-rpc.polkachu.com"
+);
+let statisticData = {};
+let LBData = {};
+
+app.use(cors());
+
+const fetchStatistic = async () => {
+  const response = await (
+    await cosmWasmClient
+  ).queryContractSmart(process.env.NEXT_PUBLIC_STEIK_ADDRESS || "", {
+    get_state: {},
+  });
+  return response?.state;
+};
+
+const fetchLBData = async () => {
+  const response = await (
+    await cosmWasmClient
+  ).queryContractSmart(process.env.NEXT_PUBLIC_STEIK_ADDRESS || "", {
+    get_claimed_list: {},
+  });
+  return response?.claimed_info;
+};
+const fetchUpdate = async () => {
+  LBData = await fetchLBData();
+  statisticData = await fetchStatistic();
+
+  setTimeout(() => {
+    fetchUpdate();
+  }, 60 * 1000);
+};
+
+fetchUpdate();
+
+// Middleware
+app.use(express.json());
+
+// Basic route for GET request
+app.get("/fetch-statistic", (req, res) => {
+  res.send(statisticData);
+});
+
+app.get("/fetch-lbdata", (req, res) => {
+  res.send(LBData);
+});
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
